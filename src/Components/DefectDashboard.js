d0a79dfa878148e9453227ebf6741b6ffe83290a
Dhiraj <dhiraj.mobicloud@gmail.com>
2023-01-18 16:14:37 +0530

    Updated DefectDashboard and  DefectDashboardStyle components

--- src/Components/DefectDashboard.js
+++ src/Components/DefectDashboard.js
@@ -1,69 +1,140 @@
-import React from 'react'
-import { DefectDashboardStyle } from './Styled-Components/DefectDashboardStyle'
+import React from "react";
+import { DefectDashboardStyle } from "./Styled-Components/DefectDashboardStyle";
 
 const DefectDashboard = () => {
   return (
-    <DefectDashboardStyle className='container-fuild'>
-      <div className='defects-repaired'>
-        <div className='defects-heading'>
-          <h2>Defects</h2>
+    <DefectDashboardStyle className="container-fuild">
+      <div className="defects-repaired">
+        {/* -------------------- Defects List -------------------- */}
+        <div className="defects-list">
+          <div className="defects-heading">
+            <h2>Defects</h2>
+          </div>
+          <div className="defect">
+            <div className="defect-name">
+              <h5>defect xyz</h5>
+            </div>
+            <div className="done">
+              <span className="btn btn-sm btn-success">Done</span>
+            </div>
+          </div>
+
+          <div className="defect">
+            <div className="defect-name">
+              <h5>defect xyz</h5>
+            </div>
+            <div className="done">
+              <span className="btn btn-sm btn-success">Done</span>
+            </div>
+          </div>
+          <div className="defect">
+            <div className="defect-name">
+              <h5>defect xyz</h5>
+            </div>
+            <div className="done">
+              <span className="btn btn-sm btn-success">Done</span>
+            </div>
+          </div>
+          <div className="defect">
+            <div className="defect-name">
+              <h5>defect xyz</h5>
+            </div>
+            <div className="done">
+              <span className="btn btn-sm btn-success">Done</span>
+            </div>
+          </div>
+          <div className="defect">
+            <div className="defect-name">
+              <h5>defect xyz</h5>
+            </div>
+            <div className="done">
+              <span className="btn btn-sm btn-success">Done</span>
+            </div>
+          </div>
         </div>
-        <div className='defects-list'>
-          <div className='defect'>
-            <div className='defect-name'> 
-                <h5>defect xyz</h5>
-            </div>
-            <div className='done'> 
-                <span className='btn btn-sm btn-success'>Done</span>
+        {/* -------------------- Repaired List -------------------- */}
+        <div className="repaired-list">
+          <div className="repaired-heading">
+            <h2>Repaired</h2>
+          </div>
+          <div className="repaired">
+            <div className="repaired-name">
+              <h5>Repaired xyz</h5>
             </div>
           </div>
-
-          <div className='defect'>
-            <div className='defect-name'> 
-                <h5>defect xyz</h5>
-            </div>
-            <div className='done'> 
-                <span className='btn btn-sm btn-success'>Done</span>
+          <div className="repaired">
+            <div className="repaired-name">
+              <h5>Repaired xyz</h5>
             </div>
           </div>
-
-          <div className='defect'>
-            <div className='defect-name'> 
-                <h5>defect xyz</h5>
-            </div>
-            <div className='done'> 
-                <span className='btn btn-sm btn-success'>Done</span>
+          <div className="repaired">
+            <div className="repaired-name">
+              <h5>Repaired xyz</h5>
             </div>
           </div>
-
-          <div className='defect'>
-            <div className='defect-name'> 
-                <h5>defect xyz</h5>
-            </div>
-            <div className='done'> 
-                <span className='btn btn-sm btn-success'>Done</span>
+          <div className="repaired">
+            <div className="repaired-name">
+              <h5>Repaired xyz</h5>
             </div>
           </div>
-
-          <div className='defect'>
-            <div className='defect-name'> 
-                <h5>defect xyz</h5>
-            </div>
-            <div className='done'> 
-                <span className='btn btn-sm btn-success'>Done</span>
+          <div className="repaired">
+            <div className="repaired-name">
+              <h5>Repaired xyz</h5>
             </div>
           </div>
-
         </div>
-
       </div>
-      <div className='add-defects'>
-
+      {/* -------------------- Add Defect section -------------------- */}
+      <div className="add-defects">
+        <div className="vehicle-information">
+          <div className="info">
+            vehicle information
+          </div>
+        </div>
+        {/* Segments */}
+        <div className="defect-segments container-fuild">
+            <div className="add-defect-heading">
+                <h2>Add Defect</h2>
+            </div>
+            <div className="segments container row g-3">
+                <div className="a-segment col-md-3">
+                    <h5>Surface RH 1</h5>
+                </div>
+                <div className="a-segment col-md-3">
+                    <h5>Surface RH 2</h5>
+                </div>
+                <div className="a-segment col-md-3">
+                    <h5>Surface RH 3</h5>
+                </div>
+                <div className="a-segment col-md-3">
+                    <h5>Surface RH 4</h5>
+                </div>
+                <div className="a-segment col-md-3">
+                    <h5>Surface RH 5</h5>
+                </div>
+                <div className="a-segment col-md-3">
+                    <h5>Surface RH 6</h5>
+                </div>
+                <div className="a-segment col-md-3">
+                    <h5>Surface RH 7</h5>
+                </div>
+                <div className="a-segment col-md-3">
+                    <h5>Surface RH 8</h5>
+                </div>
+                <div className="a-segment col-md-3">
+                    <h5>Surface RH 9</h5>
+                </div>
+                <div className="a-segment col-md-3">
+                    <h5>Surface RH 10</h5>
+                </div>
+                <div className="a-segment col-md-3">
+                    <h5>Surface RH 11</h5>
+                </div>
+            </div>
+        </div>
       </div>
-
     </DefectDashboardStyle>
-  )
-}
+  );
+};
 
-export default DefectDashboard
-
+export default DefectDashboard;
--- src/Components/Styled-Components/DefectDashboardStyle.js
+++ src/Components/Styled-Components/DefectDashboardStyle.js
@@ -2,30 +2,37 @@ import styled from 'styled-components'
 
 export const DefectDashboardStyle = styled.div`
 background-color: #E8E8E8;
-
 display: flex;
+overflow: hidden;
 
 .defects-repaired{
   width: 30%;
-  height: 100vh;
+  /* height: 100vh; */
   border-right: solid black 1px;
   display: flex;
   flex-direction: column;
+
+/*-------------------- Defects List --------------------*/
+
+.defects-list{
+  padding: 10px;
+  height: 50%;
+  overflow: scroll;
+  border-bottom: solid black 1px ;
+  ::-webkit-scrollbar {
+    display: none;
+}
 
   .defects-heading{
     height: 45px;
     h2{
     text-align: center;
-    padding: 10px;
   }
   }
-
-.defects-list{
-  padding: 10px;
 
   .defect{
     display: flex;
-    margin: 5px ;
+    margin: 3px ;
     align-items: center;
 
     .defect-name{
@@ -42,6 +49,91 @@ display: flex;
 
 }
 
+/*-------------------- Repaired List --------------------*/
+
+.repaired-list{
+    padding: 10px;
+    overflow: scroll;
+    height: 50%;
+    ::-webkit-scrollbar {
+    display: none;
+    }
+
+    .repaired-heading{
+        height: 45px;
+        h2{
+        text-align: center;
+        }
+    }
+
+    .repaired{
+        display: flex;
+        margin: 10px ;
+        align-items: center;
+       
+        .repaired-name{
+            width: 683px;
+            background-color: #9CF27E;
+            border-radius: 5px;
+            padding-left: 10px;
+        }
+    }
 }
+
+}
+
+/* -------------------- Add Defect section -------------------- */
+
+.add-defects{
+    display: flex;
+    flex-direction: column;
+    /* width: 100vw; */
+
+    .vehicle-information{
+        height: 30%;
+        display: flex;
+        
+        .info{
+            height: 200px;
+            width: 700px;
+            margin: 20px auto;
+            background-color: #C6C0C0;
+        }
+    }
+
+    /* Segments */
+
+    .defect-segments{
+        height: 70%;
+        margin-top: 50px;
+        .add-defect-heading{
+            height: 45px;
+                h2{
+                    text-align: center;
+                    color: #008EAD;
+                }
+            }
+        }
+
+        .segments{
+            padding: 20px;
+
+            .a-segment{
+                display: flex;
+                width: 185px;
+                height: 60px;
+                margin: 20px auto;
+                background-color: #58C8EB;
+                border-radius: 10px;
+
+                h5{
+                    margin: auto;
+                    text-align: center;
+                }
+            }
+        }
+
+    }
+
 
 `;
