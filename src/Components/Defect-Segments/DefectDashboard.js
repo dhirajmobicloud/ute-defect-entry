00ced5839a12700b83c7fafea290e3039b04bc25
atulShinde <atul.mobicloud@gamil.com>
2023-02-08 17:14:55 +0530

    Small Changes

--- src/Components/Defect-Segments/DefectDashboard.js
+++ src/Components/Defect-Segments/DefectDashboard.js
@@ -171,73 +171,73 @@ const DefectDashboard = () => {
           <div className="segments  ">
             <div className="inner-segment d-flex container row g-3">
               <div
-                className="a-segment col-md-3 border border-info"
+                className="a-segment col-md-3"
                 onClick={() => navigate("/surface-RH-139-defects")}
               >
                 <h5 >Surface RH 139</h5>
               </div>
               <div
-                className="a-segment col-md-3 border border-info"
+                className="a-segment col-md-3"
                 onClick={() => navigate("/surface-FTR-139-defects")}
               >
                 <h5>Surface FTR 139</h5>
               </div>
               <div
-                className="a-segment col-md-3 border border-info"
+                className="a-segment col-md-3"
                 onClick={() => navigate("/bluetooth-139-defect")}
               >
                 <h5>Bluetooth 139</h5>
               </div>
               <div
-                className="a-segment col-md-3 border border-info"
+                className="a-segment col-md-3 "
                 onClick={() => navigate("/electrical-1-140-defects")}
               >
                 <h5>Electrical 1 140</h5>
               </div>
               <div
-                className="a-segment col-md-3 border border-info"
+                className="a-segment col-md-3 "
                 onClick={() => navigate("/surface-LH-140-defect")}
               >
                 <h5>Surface LH 140</h5>
               </div>
               <div
-                className="a-segment col-md-3 border border-info"
+                className="a-segment col-md-3 "
                 onClick={() => navigate("/rear-INT-140-defects")}
               >
                 <h5>Rear Int 140</h5>
               </div>
               <div
-                className="a-segment col-md-3 border border-info"
+                className="a-segment col-md-3 "
                 onClick={() => navigate("/rear-EXT-141-defects")}
               >
                 <h5>Rear EXT 141</h5>
               </div>
               <div
-                className="a-segment col-md-3 border border-info"
+                className="a-segment col-md-3 "
                 onClick={() => navigate("/rh-exterior-141-defects")}
               >
                 <h5>RH Exterior 141</h5>
               </div>
               <div
-                className="a-segment col-md-3 border border-info"
+                className="a-segment col-md-3 "
                 onClick={() => navigate("/lh-exterior-141-defects")}
               >
                 <h5>LH Exterior 141</h5>
               </div>
               <div
-                className="a-segment col-md-3 border border-info"
+                className="a-segment col-md-3 "
                 onClick={() => navigate("/electrical-2-142-defects")}
               >
                 <h5>Electrical 2 142</h5>
               </div>
               <div
-                className="a-segment col-md-3 border border-info"
+                className="a-segment col-md-3 "
                 onClick={() => navigate("/front-EXT-142-defects")}
               >
                 <h5>Front EXT 142</h5>
               </div>
               <div
-                className="a-segment col-md-3 border border-info"
+                className="a-segment col-md-3 "
                 onClick={() => navigate("/door-closing-142-defects")}
               >
                 <h5>Door Closing 142</h5>
